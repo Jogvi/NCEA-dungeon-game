365a1851be58befb32265fb98a888e891fcf25bf
Jogvi <95467397+Jogvi@users.noreply.github.com>
2022-06-10 12:26:15 +1200

    Equipment nearly working !
    >.<

--- NCEA dungeon game/NCEA dungeon game.py
+++ NCEA dungeon game/NCEA dungeon game.py
@@ -26,28 +26,7 @@ stock_dict = {
     10 : 'Stages\\Shop\\Forge_stocks.txt',
     }
 
-price_dict={
-    'Small Amulet of strength' : 50,
-    'Medium Amulet of strength'  :100,
-    'Large Amulet of strength' : 200,
-    'Small Amulet of Defense' : 50,
-    'Medium Amulet of Defense' : 100,
-    'Large Amulet of Defense' : 200,
-    'Small Amulet of Healing' : 100,
-    'Medium Amulet of Healing':200,
-    'Large Amulet of Healing':200,
-    }
-
 item_types={
-    'Small Amulet of strength' :  5,
-    'Medium Amulet of strength': 5,
-    'Large Amulet of strength' : 5,
-    'Small Amulet of Defense' : 5,
-    'Medium Amulet of Defense' : 5,
-    'Large Amulet of Defense' : 5,
-    'Small Amulet of Healing' : 5,
-    'Medium Amulet of Healing' : 5,
-    'Large Amulet of Healing' : 5,
     'Shortbow':6,
     'Bow' : 6,
     'Longbow':6,
@@ -85,6 +64,7 @@ def load(file):
     return obj
  
 def load_game():
+    global player
     slot=slot_dict[int(input('What slot would you like to load?'))]
     with open(slot, 'rb') as data:
         player = p.load(data)
@@ -104,7 +84,7 @@ def menu():
     action=input(f'You now have {player.health} health points left. Would you like to delve '\
           'deeper in the dungeon, or go home? You can also choose to save by '\
           f'typing "{save_inputs[0]}" or "{save_inputs[1]}"'\
-          f'and load with "{load_inputs[0]}" or "{load_inputs[1]}"')
+          f'and load with "{load_inputs[0]}" or "{load_inputs[1]}" \n')
     if action in delve_inputs:
         delve()
     elif action in home_inputs:
@@ -119,7 +99,7 @@ def menu():
         menu()
 def delve():
     stage=r.randint(0,100)
-    if stage<=90:
+    if stage<=9:
         monster=r.randint(1,3)
         if monster==1:
             print(f'A wild {goblin.name} appears!')
@@ -162,27 +142,35 @@ def fight(monster):
                 print(f'The {monster.name} dropped {monster.money} copper pieces')
             menu()
 def equip_item(item):
-    if item_types[item]==1:
-        player.headwear=armour_dict[item]
-    elif item_types[item]==2:
-        player.body_armour=armour_dict[item]
-    elif item_types[item]==3:
-        player.pants=armour_dict[item]
-    elif item_types[item]==4:
-        player.footwear=armour_dict[item]
-    elif item_types[item]==5:
-        action=int(input(f'Would you like to equip your {item} as a '\
+    if item.type == 0:
+        player.equipped_weapon = item
+        menu()
+    elif item.type == 1:
+        player.headwear = item
+        menu()
+    elif item.type == 2:
+        player.body_armour = item
+        menu()
+    elif item.type ==3:
+        player.pants = item
+        menu()
+    elif item.type == 4:
+        player.footwear = item
+        menu()
+    elif item.type==5:
+        action=int(input(f'Would you like to equip your {item.name} as a '\
                      'right wrist bracelet(1), a left wrist bracelet(2) or'\
                      'a necklace(3)?'))
         if action==1:
-            player.bracelet1=equipment_dict[item]
+            player.bracelet1=item
             print(f"Your new right bracelet is a {player.bracelet1.name}")
         elif action==2:
-            player.bracelet2=equipment_dict[item]
+            player.bracelet2=item
             print(f"Your new left bracelet is a {player.bracelet2.name}")
         elif action == 3:
-            player.necklace=equipment_dict[item]
+            player.necklace=item
             print(f"Your new necklace is a {player.necklace.name}")
+        menu()
 
 def bank():
     if input('would you like to use our services?') in yes_inputs:
@@ -200,20 +188,28 @@ def bank():
                 input('[Enter] to continue')
                 bank()
         elif action==2:
-             if player.money_bank ==0:
-                 print("You don't even have money here! Please deposit before you start withdrawing...")
-             withdrawal_amount=int(input("how much would you like to withdraw"))
-             if withdrawal_amount<=player.bank_money:
+            if player.money_bank ==0:
+                print("You don't even have money here! Please deposit before you start withdrawing...")
+            withdrawal_amount=int(input("how much would you like to withdraw"))
+            if withdrawal_amount<=player.bank_money:
                 player.bank_money-=player.bank_money
-            else:
-                if player.bank_money>0:
-                    print(f'Sorry, your bank account only has {player.bank_money} ducats')
-                               
+        else:
+            if player.bank_money>0:
+                print(f'Sorry, your bank account only has {player.bank_money} ducats')
+def object_to_title(item):
+    out = ""
+    for i in range(0, len(item)): 
+        if item[i] != "_": 
+            out = out + item[i]
+        else:
+            out = out + " "
+    out = out.title()
+    return out
 
 def shop():
     shopkeeper_id=r.randint(10,19)
     shop_id=r.randint(1,9)
-    shop_id=6
+    shop_id=8
     with open('Stages\\Shop\\Shops.txt','r') as f:
         content=f.readlines()
         shop_name=content[shop_id-1].strip()
@@ -224,41 +220,48 @@ def shop():
     print(f"welcome to {shopkeeper}'s {shop_name}. I am {shopkeeper}.")
     print(shop_description)
     input('Press [Enter] to continue')
+    #if shop is inferior to 3 it is a service
     if shop_id > 3:
         print('this is what we have on sale')
         shop_stock=stock_dict[shop_id]
         with open(shop_stock,'r') as f:
             content=f.readlines()
+        item_names=[]
+        for i in range (0, len(content)):
+            item_names.append(object_to_title(content[i]))
+            
         items_on_sale=[]
-        for i in range (0,3):
-            items_on_sale.append(content[r.randint(0,len(content)-1)].strip())
-        for i in range (0,len(items_on_sale)):
-            print(items_on_sale[i],':',price_dict[items_on_sale[i]])
+        loop = 0
+        while loop <3:
+            item = content[r.randint(0,len(content)-1)].strip()
+            if eval(item).level<=player.level:
+                items_on_sale.append(item)
+                print(f'{loop+1}:{eval(item).name} : {eval(item).price}')
+                loop+=1
         action=input('Would you like to buy something?You currently have '\
-                     f'{player.money} Ducats')
+                     f'{player.money} Ducats \n')
         if action in yes_inputs:
-            action=int(input('What would you like to buy? (1,2 or 3)'))
+            action=int(input('What would you like to buy? (1,2 or 3)\n'))
             global item_bought
-            item_bought=items_on_sale[action-1]
-            item_price=price_dict[item_bought]
-            if item_price <= player.money:
-                player.money-=item_price
-                action=input(f'Do you want to equip your new {item_bought}?')
+            item = eval(item)
+            if item.price <= player.money:
+                player.money-=item.price
+                action=input(f'Do you want to equip your new {item.name}? \n')
                 if action in yes_inputs:
-                    equip_item(item_bought)
+                    equip_item(item)
                 elif action in no_inputs:
-                    player.backpack.append(item_bought)
+                    player.backpack.append(item)
                 else:
                     print("I think you've misspelled your input")
             else:
                 print('sorry, you dont have enough money...')
-                menu()
+                shop()
         else:
             print('See you soon!')
             menu()
     else:
         action=input('Would you like to use our services?'\
-                     f'You currently have {player.money} Ducats')
+                     f'You currently have {player.money} Ducats \n')
         if action in yes_inputs:
             print('Services will be available soon')
             menu()
@@ -268,12 +271,16 @@ def healer():
     menu()
 
 def game_over():
-    action=input('would you like to save your score and name?')
+    action=input('would you like to save your score and name? \n')
     if action in yes_inputs:
+        with open('Save_Files\\Saved_Scores.txt','r') as f:
+            content = f.readlines()
         with open('Save_Files\\Saved_Scores.txt','w') as f:
-            f.write(str(player.score))
+            f.write(' '.join(content))
             f.write('\n')
-            f.write(player.name)
+            f.write(str(player.score).strip())
+            f.write('\n')
+            f.write(player.name.strip())
 
 class Lv1_ennemies:
     def __init__(self,name,damage,max_health,money):
@@ -288,39 +295,51 @@ goblin=Lv1_ennemies('goblin',5,20,5)
 bat=Lv1_ennemies('bat',1,1,1)
 skeleton=Lv1_ennemies('skeleton',8,10,10)
 
+class Helmet:
+    def __intit__(self, name, armour, solidity, level):
+        self.name=name
+        self.armour=armour
+        self.solidity=solidity
+        self.level = level
+        self.type = 1
+
 class Weapon:
-    def __init__(self,name,damage,solidity, level):
+    def __init__(self,name,damage,solidity, level,price):
         self.name=name
         self.damage=damage
         self.solidity=solidity
         self.level = level
-basic_sword = Weapon('Basic Sword',15,50, 0)
-bronze_broadswoard = Weapon('Bronze Broadswoard' , 20,  100,1)
-silver_broadswoard = Weapon('Silver Broadswoard', 30, 150,1)
-gold_broadswoard = Weapon('Gold Broadswoard', 45, 80,2)
-platinum_broadswoard = Weapon('Platinum Broadswoard', 60 , 100,2)
-bronze_battleaxe = Weapon('Bronze Battleaxe', 30,  150, 2)
-silver_battleaxe = Weapon('Silver Battleaxe', 50, 200,2)
-gold_battleaxe = Weapon('Gold Battleaxe', 75, 100, 2)
-platinum_battleaxe = Weapon('Platinum Battleaxe',100,125,3)
+        self.type = 0
+        self.price = price
+basic_sword = Weapon('Basic Sword',15,50, 0,0)
+bronze_broadswoard = Weapon('Bronze Broadswoard' , 20,  100,1,100)
+silver_broadswoard = Weapon('Silver Broadswoard', 30, 150,1,200)
+gold_broadswoard = Weapon('Gold Broadswoard', 45, 80,2,300)
+platinum_broadswoard = Weapon('Platinum Broadswoard',60,100,2,500)
+bronze_battleaxe = Weapon('Bronze Battleaxe',30,150,2,150)
+silver_battleaxe = Weapon('Silver Battleaxe',50,200,2,300)
+gold_battleaxe = Weapon('Gold Battleaxe', 75, 100, 2,500)
+platinum_battleaxe = Weapon('Platinum Battleaxe',100,125,3,1000)    
 
 class Amulet:
-    def __init__(self,damage,defense,healing,name):
+    def __init__(self,damage,defense,healing,name, price):
         self.damage=damage
         self.defense=defense
         self.healing=healing
+        self.price = price
         self.name=name
+        self.type = 5
 
-null_amulet=Amulet(0,0,0,'nothing')
-small_amulet_strength=Amulet(3,0,0,'Small Amulet of Strength')
-medium_amulet_strength=Amulet(5,0,0,'Medium Amulet of Strength')
-large_amulet_strength=Amulet(8,0,0,'Large Amulet of Strength')
-small_amulet_defense=Amulet(0,3,0,'Small Amulet of Defense')
-medium_amulet_defense=Amulet(0,5,0,'Medium Amulet of Defense')
-large_amulet_defense=Amulet(0,8,0,'Large Amulet of Defense')
-small_amulet_healing=Amulet(0,0,3,'Small Amulet of Healing')
-medium_amulet_healing=Amulet(0,0,5,'Medium Amulet of Healing')
-large_amulet_healing=Amulet(0,0,8,'Large Amulet of Healing')
+null_amulet=Amulet(0,0,0,'nothing',0)
+small_amulet_strength=Amulet(3,0,0,'Small Amulet of Strength',50)
+medium_amulet_strength=Amulet(5,0,0,'Medium Amulet of Strength',100)
+large_amulet_strength=Amulet(8,0,0,'Large Amulet of Strength',200)
+small_amulet_defense=Amulet(0,3,0,'Small Amulet of Defense',50)
+medium_amulet_defense=Amulet(0,5,0,'Medium Amulet of Defense',100)
+large_amulet_defense=Amulet(0,8,0,'Large Amulet of Defense',200)
+small_amulet_healing=Amulet(0,0,3,'Small Amulet of Healing',50)
+medium_amulet_healing=Amulet(0,0,5,'Medium Amulet of Healing',100)
+large_amulet_healing=Amulet(0,0,8,'Large Amulet of Healing',200)
 
 #Dictionnary is here because otherwhise the objects are undefined
 equipment_dict={
@@ -333,6 +352,14 @@ equipment_dict={
     'Small Amulet of Healing':small_amulet_healing,
     'Medium Amulet of Healing':medium_amulet_healing,
     'Large Amulet of Healing':large_amulet_healing,
+    'Bronze Broadswoard' : bronze_broadswoard,
+    'Silver Broadswoard' : silver_broadswoard,
+    'Gold Broadswoard' : gold_broadswoard,
+    'Platinum Broadswoard' : platinum_broadswoard,
+    'Bronze Battleaxe' : bronze_battleaxe,
+    'Silver Battleaxe' : silver_battleaxe,
+    'Gold Battleaxe' : gold_battleaxe,
+    'Platinum Battleaxe' : platinum_battleaxe,
 }
 
 class Player:
@@ -355,8 +382,11 @@ class Player:
         self.equipped_weapon = basic_sword
         self.temp_damage=damage*self.equipped_weapon.damage
         self.bank_money=0
-        
+        self.level=1
+
 print('Welcome to the dungeon of rickrollia!')
-player=Player(input('What is your name, fellow adventurer?'),1,50,50)
+player=Player(input('What is your name, fellow adventurer?').title().lstrip(),1,50,350)
 
 menu()
+
+
